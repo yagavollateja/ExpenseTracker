dcf480e832876116c5a67dabe3f9a8d4e1f29ddc
YAGAVOLLA TEJA <yagavollateja@gmail.com>
2026-01-04 18:52:20 +0530

    Refactor jwtManager to check for JWT_SECRET

--- server/managers/jwt_manger.js
+++ server/managers/jwt_manger.js
@@ -1,28 +1,23 @@
-// const jsonwebtoken = require("jsonwebtoken");
-
-// const jwtManager = (user)=>{
-//     const acceesToken = jsonwebtoken.sign(
-//         {
-//             _id:user.id,
-//             name:user.name,
-//         },
-//         procees.env.jsw_salt;
-//     )
-//     return acceesToken;
-// };
-// module.exports = jwtManager;
 const jwt = require("jsonwebtoken");
 
 const jwtManager = (user) => {
-    const accessToken = jwt.sign(
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+
+  const accessToken = jwt.sign(
     {
-        _id: user._id,
-        name: user.name,
+      _id: user._id,
+      name: user.name,
     },
-    process.env.JWT_SALT, // make sure this exists in your .env file
-    { expiresIn: "1d" }   // optional expiry
-    );
-    return accessToken;
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d", // ✅ token valid for 1 day
+    }
+  );
+
+  return accessToken;
 };
 
 module.exports = jwtManager;
+
